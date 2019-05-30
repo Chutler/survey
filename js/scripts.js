2f9f05de54d870eb84b93934c0d13026d2ade07f
Craig Hutler and Vilija Jozaitis <craig@>
2019-05-29 20:38:55 -0700

    fix link to script

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
+  console.log("ready")
+  $("#survey").submit(function(event) {
 
-  $("#blanks form").submit(function(event) {
     alert('Got to beginning of form submit!');
+
     var firstNameInput = $("input#firstName").val();
     var lastNameInput = $("input#lastName").val();
-
-    // Questionable
     var flavorInput = $("input:radio[name=flavor]:checked").val();
     var dobInput = $("input#born").val();
     var beverageInput = $("input#beverage").val();
@@ -13,12 +13,13 @@ $(document).ready(function() {
 
     $(".firstName").append(firstNameInput);
     $(".lastName").append(lastNameInput);
-    // Questionable
     $(".flavor").append(flavorInput);
     $(".dob").append(dobInput);
     $(".beverage").append(beverageInput);
     $(".color").append(favoriteColorInput);
+
     alert("Maybe?")
+
     $("#response").show();
 
     event.preventDefault();
